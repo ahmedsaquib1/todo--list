86799e70cc194e6f281d85e20a9740a3f444ec52
ahmedsaquib1 <saquibahmed@excellencetechnologies.in>
2022-01-19 13:22:42 +0530

    complete todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Guid } from "guid-typescript";
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  todos: Todo[] = [
+    new Todo(Guid.create(), 'Eat Breakfast',false),
+    new Todo(Guid.create(),'Pay Bill', true),
+    new Todo(Guid.create(),'Feed Cat', false),
+  ]
+
+  onSubmit(form: NgForm){
+    let todo= new Todo(Guid.create(),form.value.title, false);
+    this.todos.push(todo);
+    form.resetForm();
+  }
+  onComplete(id:Guid){
+    let todo = this.todos.filter(x=>x.id === id)[0];
+    todo.isComplete=true;
+
+  }
+  onDelete(id:Guid){
+    let todo= this.todos.filter(x=>x.id === id)[0];
+let index= this.todos.indexOf(todo,0);
+if(index > -1){
+  this.todos.splice(index,1);
+}
+
+  }
+ 
 }
